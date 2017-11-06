2d7630f5d7fbe7e7c00a9996918887c6ed829270
Derek <mail@derek.tech>
2017-11-06 12:23:33 +0000

    Added gitattributes and rejigged setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python3
 
 import os
+import os.path
 import shutil
 
-print(os.getcwd())
-os.symlink('lincolntex/Makefile', 'Makefile')
-os.symlink('lincolntex/gitignore-project', '.gitignore')
-shutil.copyfile('lincolntex/report.tex', 'report.tex')
-shutil.copyfile('lincolntex/lincoln.cls', 'lincoln.cls')
-open('report.bib', 'a').close()
-os.makedirs('images')
+makefile_path = 'Makefile'
+gitignore_path = '.gitignore'
+gitattributes_path = '.gitattributes'
+report_path = 'report.tex'
+class_path = 'lincoln.cls'
+bib_path = 'lincoln.bib'
+images_path = 'images'
+
+if not os.path.isfile(makefile_path):
+    os.symlink('lincolntex/Makefile', makefile_path)
+
+if not os.path.isfile(gitignore_path):
+    os.symlink('lincolntex/gitignore-project', gitignore_path)
+
+if not os.path.isfile(gitattributes_path):
+    os.symlink('lincolntex/gitattributes-project', gitattributes_path)
+
+if not os.path.isfile(report_path):
+    shutil.copyfile('lincolntex/{}'.format(report_path), report_path)
+
+if not os.path.isfile(class_path):
+    shutil.copyfile('lincolntex/{}'.format(class_path), class_path)
+
+if not os.path.isfile(bib_path):
+    open('report.bib', 'a').close()
+
+if not os.path.isdir(images_path):
+    os.makedirs('images')
